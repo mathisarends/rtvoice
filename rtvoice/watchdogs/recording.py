2c58b7a61fbebf09b73c137a48051e00305c6bf4
mathisarends <mathisarends27@gmail.com>
2026-02-27 21:23:28 +0100

    Remove recording watchdog stub

--- rtvoice/watchdogs/recording.py
+++
@@ -1,9 +0,0 @@
-# from rtvoice.events.bus import EventBus
-
-
-"""class RecordingWatchdog:
-def __init__(self, event_bus: EventBus) -> None:
-    self._event_bus = event_bus
-    self._event_bus.subscribe(
-        InputAudioBufferAppendEvent, self._on_input_audio_buffer_append
-    )"""
